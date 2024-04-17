0ba2c2f0eb784ca61d2b07f249f65e9595bbf241
Kayla C <itskaychay@gmail.com>
2024-04-17 13:34:25 -0400

    updated

---
+++ script.js
@@ -0,0 +1,20 @@
+const card1 = document.getElementById("default-card-one");
+card1.removeAttribute("src");
+card1.attr("src", image.jpg); // card div
+const card2 = document.getElementById("default-card-two"); // card div
+const card3 = document.getElementById("default-card-three"); // card div
+const card4 = document.getElementById("default-card-four"); // card div
+const card5 = document.getElementById("default-card-five"); // card div
+const card6 = document.getElementById("default-card-six"); // card div
+const card7 = document.getElementById("default-card-seven"); // card div
+const card8 = document.getElementById("default-card-eight"); // card div
+
+const cards = [card1, card2, card3, card4, card5, card6, card7, card8];
+
+cards.forEach((card) =>
+  card.addEventListener("click", () => {
+    //   console.log("test");
+    card.classList.add("remove-card");
+    setTimeout(() => card.classList.remove("remove-card"), 1000);
+  })
+);
